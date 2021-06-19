5a5cdb69c7cfc852d11f13068385a9516f49d3ad
millnitzluan <luanmillnitz@gmail.com>
2021-06-19 15:55:37 -0300

    move island_engine

---
+++ lib/islands_engine/rules.ex
@@ -0,0 +1,12 @@
+defmodule IslandEngine.Rules do
+  alias __MODULE__
+
+  defstruct state: :initialized
+
+  def new(), do: %Rules{}
+
+  def check(_state, _action), do: :error
+
+  def check(%Rules{state: :initialized} = rules, :add_player), do:
+    {:ok, %Rules{rules | state: :players_set}}
+end
