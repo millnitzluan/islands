0d3838215032b26d1d388414636b8e525a13a1f5
millnitzluan <luanmillnitz@gmail.com>
2021-06-22 23:47:28 -0300

    start with supervisor

--- lib/islands_engine/application.ex
+++ lib/islands_engine/application.ex
@@ -8,7 +8,8 @@ defmodule IslandsEngine.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Registry, keys: :unique, name: Registry.Game}
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
---
+++ lib/islands_engine/game_supervisor.ex
@@ -0,0 +1,26 @@
+defmodule IslandsEngine.GameSupervisor do
+  use Supervisor
+
+  alias IslandsEngine.Game
+
+  def init(:ok) do
+    Supervisor.init([Game], strategy: :simple_one_for_one)
+  end
+
+  def start_link(_options) do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def start_game(name), do:
+  Supervisor.start_child(__MODULE__, [name])
+
+  def stop_game(name) do
+    Supervisor.terminate_child(__MODULE__, pid_from_name(name))
+  end
+
+  defp pid_from_name(name) do
+    name
+    |> Game.via_tuple()
+    |> GenServer.whereis()
+  end
+end
